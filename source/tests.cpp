2cc3ac430f795f848ac6b773fb2a28e48bb5fbbb
heinrich-foto <github@heinrich-foto.de>
2015-05-02 13:43:57 +0200

    rectangle tests

--- source/tests.cpp
+++ source/tests.cpp
@@ -4,6 +4,7 @@
 
 #include "point2D.hpp"
 #include "circle.hpp"
+#include "rectangle.hpp"
 
 
 // Test Case Aufgabe 2.3
@@ -88,6 +89,43 @@ TEST_CASE("Cirlce - area","[circle.aria()]")
     c.area(0.3);
     REQUIRE(c.area() == Approx(0.3));
 }
+
+TEST_CASE("Circle - circumference","[circle.circumference()]")
+{
+    Circle c{Point2D (0.3,0, Color{}) ,3.939};
+    c.circumference(1.0);
+    REQUIRE(c.radius() == Approx(0.15915));
+    c.radius(393.9);
+    REQUIRE(c.circumference() == Approx(2474.94));
+}
+
+TEST_CASE("Circle is inside","[circle.is_inside()]")
+{
+    Circle c {Point2D(3,3),1};
+    REQUIRE(c.is_inside({2.5,2.5}));
+}
+
+
+TEST_CASE("Rectangle - constuctor","[rectangle()]")
+{
+    Rectangle rect{};
+    REQUIRE(rect.width() == 0.1);
+}
+
+TEST_CASE("Rectangle - translate","[rectangle.translate()]")
+{
+    Rectangle rect{Point2D (0.2,0.1, Color (1.0,0.0,0.0)),0.2,0.2};
+    rect.translate(0.1,0.2);
+    REQUIRE(rect.fixpoint().x() == Approx(0.3));
+    REQUIRE(rect.fixpoint().y() == Approx(0.3));
+}
+
+TEST_CASE("Rectangle - is inside","[rectangle.is_inside()]")
+{
+    Rectangle rect { Point2D{0.2,0.1,{1.0,0.0,0.0}},0.2,0.2};
+    REQUIRE(rect.is_inside({0.3,0.2}));
+}
+
 
 int main(int argc, char *argv[])
 {
